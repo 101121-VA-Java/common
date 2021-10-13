fa4570af6b0b83d88166657880f4da30c9f5a13f
Priyamvadha Sreenivasan <crm4803@gmail.com>
2021-10-13 14:53:31 -0400

    new java file

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld{
+  public static void main(String[] args){
+    int a = 10;
+    int b = 10;
+    
+    if(a==b){
+      System.out.println("Hello World!");
+    
+    }else{
+      System.out.println("Hello Earth!");
+    }
+  }
+}
