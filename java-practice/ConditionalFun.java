d526ced88af8ef74205f5247e5570bcc3d6bbf6a
antwanette <lillefty01@aol.com>
2021-10-13 15:16:05 -0400

    Control flow statements practice

---
+++ java-practice/ConditionalFun.java
@@ -0,0 +1,16 @@
+
+public class ConditionalFun {
+
+	public static void main(String[] args){
+		int t=32;
+
+		if (t < 40){
+		System.out.println ("Wear a parka!");
+		}
+
+		else if (t>=40) {
+		System.out.println("Wear a light jacket.");
+		}
+
+} //end main
+}
